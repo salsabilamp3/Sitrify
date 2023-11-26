b384ebdf9d690a4ffcfc2e8e838760b915ec46d3
salsabilamp3 <salsabilamp3@gmail.com>
2023-11-26 16:38:32 +0700

    add fetch artist' songs

--- backend/app/routes/songs_route.py
+++ backend/app/routes/songs_route.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.controllers.songs_controller import get_all_artists_songs, get_song_audio_features
+from app.controllers.songs_controller import get_all_artists_songs, get_song_audio_features, get_song_by_id, predict_song
 
 songs_route = Blueprint('songs_route', __name__, url_prefix='/api')
 
@@ -8,7 +8,28 @@ def get_songs(id_artist):
     songs = get_all_artists_songs(id_artist)
     return jsonify(songs)
 
-@songs_route.route('/songs/audio-features/<id_song>', methods=['GET'])
+@songs_route.route('/song/<id_song>', methods=['GET'])
+def get_song(id_song):
+    song = get_song_by_id(id_song)
+    return jsonify(song)
+
+@songs_route.route('/song/audio-features/<id_song>', methods=['GET'])
 def get_audio_features(id_song):
     audio_features = get_song_audio_features(id_song)
     return jsonify(audio_features)
+
+@songs_route.route('/song/predict/<id_song>', methods=['GET'])
+def predict(id_song):
+    try:
+        audio_features = get_song_audio_features(id_song)
+        prediction = predict_song(audio_features)
+        result = {"prediction": bool(prediction)}
+
+        return jsonify(result)
+
+    except Exception as e:
+        # Log the exception for debugging purposes
+        print(f"Error predicting song: {str(e)}")
+
+        # Return an error response
+        return jsonify({"error": "Internal Server Error"}), 500
--- frontend/src/pages/prediksiPage.jsx
+++ frontend/src/pages/prediksiPage.jsx
@@ -1,10 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SideBar from "../components/sidebar";
-import { DataTable } from "simple-datatables";
 import AvatarProfile from "../components/profile";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const PrediksiPage = () => {
-  useEffect(() => {});
+  const [id_artist, setIdArtist] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [songsData, setSongsData] = useState([]);
+  const [predictResult, setPredictResult] = useState("");
+
+  useEffect(() => {
+    const id = localStorage.getItem("id_artist");
+    GetDataSongs(id);
+  }, []);
+
+  const GetDataSongs = async (id_artist) => {
+    try {
+      setIsLoading(true);
+      if (!id_artist) {
+        return;
+      }
+      const songsResponse = await fetch(
+        `http://localhost:5000/api/songs/${id_artist}`
+      );
+      const songsData = await songsResponse.json();
+      setSongsData(songsData.songs);
+
+      console.log(songsData.songs);
+    } catch (error) {
+      console.error("Error fetching chart data:", error.message);
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1500);
+    }
+  }
+
+  const formatDate = (inputDate) => {
+    const date = new Date(inputDate);
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    return new Intl.DateTimeFormat('id-ID', options).format(date);
+  };
+
+  const handlePredict = async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/predict/${id_artist}`);
+      const data = await response.json();
+      if (response.ok) {
+        setPredictResult(data.prediction);
+        console.log("Predict result:", data.prediction);
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }
 
   return (
     <div className="flex">
@@ -16,103 +65,109 @@ const PrediksiPage = () => {
         </div>
         <div className="pl-4">
           <div className="card bg-[#453158] shadow-xl">
-            <div className="card-body p-0 bg-[#453158]">
+            <div className="card-body p-0">
               <h2 className="card-title pb-2 text-white">Daftar Lagu</h2>
-              <div className="bg-white overflow-x-auto ">
-                <table cellPadding={0} cellSpacing={0} className="table">
+              <div className="bg-[#453158] overflow-x-auto ">
+                {isLoading ? (
+                  <div className="w-full flex items-center justify-center text-white">
+                    <AiOutlineLoading3Quarters
+                      className="animate-spin mr-2"
+                    />
+                    Loading ...
+                  </div>
+                ) : (
+                <table cellPadding={0} cellSpacing={0} className="table rounded-md border-separate datatable">
                   {/* head */}
-                  <thead>
-                    <tr className="text-white">
+                  <thead className="text-white bg-[#29163a] text-sm">
+                    <tr>
                       <th>#</th>
                       <th>Judul Lagu</th>
                       <th>Tanggal Rilis</th>
                       <th>Aksi</th>
                     </tr>
                   </thead>
-                  <tbody className="bg-[#453158]">
-                    {/* row 1 */}
-                    <tr className="text-white">
-                      <td>1</td>
-                      <td>
-                        <div className="flex items-center space-x-3">
-                          <div className="avatar">
-                            <div className="mask mask-squircle w-12 h-12">
-                              <img
-                                src="/image/profile.jpg"
-                                alt="Avatar Tailwind CSS Component"
-                              />
-                            </div>
-                          </div>
-                          <div>
-                            Zemlak, Daniel and Leannon
-                            <br />
-                            <span className="badge badge-ghost badge-sm">
-                              Desktop Support Technician
-                            </span>
-                          </div>
-                        </div>
-                      </td>
-                      <td>Zemlak, Daniel and Leannon</td>
-                      <td>
-                        {" "}
-                        <button
-                          className="btn btn-primary btn-sm"
-                          onClick={() =>
-                            document
-                              .getElementById("modal_prediksi")
-                              .showModal()
-                          }
-                        >
-                          Prediksi
-                        </button>
-                        {/* Modal hasil prediksi */}
-                        <dialog id="modal_prediksi" className="modal">
-                          <div
-                            className="modal-box"
-                            style={{
-                              backgroundColor: "#EADCF8",
-                              color: "#29163A",
-                            }}
-                          >
-                            <form method="dialog">
-                              {/* if there is a button in form, it will close the modal */}
-                              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                                ✕
-                              </button>
-                            </form>
-
-                            <h3 className="font-bold text-2xl text-center p-5 underline">
-                              Hasil Prediksi
-                            </h3>
-                            <div className="ml-4">
-                              <p className="py-4 font-bold text-xl">
-                                Judul Lagu
-                              </p>
-                              <div className="flex items-center space-x-3">
-                                <div className="avatar">
-                                  <div className="mask mask-squircle w-12 h-12">
-                                    <img
-                                      src="/image/profile.jpg"
-                                      alt="Avatar Tailwind CSS Component"
-                                    />
-                                  </div>
-                                </div>
-                                <div>
-                                  <p className="text-md">
-                                    Zemlak, Daniel and Leannon
-                                  </p>
-                                </div>
+                  <tbody style={{ color: "#29163A" }}>
+                    {songsData.map((song, index) => (
+                      <tr className="text-white" key={song.id_song}>
+                        <td>{index + 1}</td>
+                        <td>
+                          <div className="flex items-center space-x-3">
+                            <div className="avatar">
+                              <div className="mask mask-squircle w-12 h-12">
+                                <img
+                                  src={song.image}
+                                  alt={song.title}
+                                />
                               </div>
                             </div>
-                            <h3 className="font-bold text-3xl text-center p-10 ">
-                              Lagu akan Hits!
-                            </h3>
+                            <div>
+                              {song.title}
+                            </div>
                           </div>
-                        </dialog>
-                      </td>
-                    </tr>
+                        </td>
+                        <td>{formatDate(song.release_date)}</td>
+                        <td>
+                          {" "}
+                          <button
+                            className="btn btn-primary btn-sm"
+                            onClick={() =>
+                              document
+                                .getElementById("modal_prediksi")
+                                .showModal()
+                            }
+                          >
+                            Prediksi
+                          </button>
+                          {/* Modal hasil prediksi */}
+                          <dialog id="modal_prediksi" className="modal">
+                            <div
+                              className="modal-box"
+                              style={{
+                                backgroundColor: "#EADCF8",
+                                color: "#29163A",
+                              }}
+                            >
+                              <form method="dialog">
+                                {/* if there is a button in form, it will close the modal */}
+                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                                  ✕
+                                </button>
+                              </form>
+
+                              <h3 className="font-bold text-2xl text-center p-5 underline">
+                                Hasil Prediksi
+                              </h3>
+                              <div className="ml-4">
+                                <p className="py-4 font-bold text-xl">
+                                  Judul Lagu
+                                </p>
+                                <div className="flex items-center space-x-3">
+                                  <div className="avatar">
+                                    <div className="mask mask-squircle w-12 h-12">
+                                      <img
+                                        src="/image/profile.jpg"
+                                        alt="Avatar Tailwind CSS Component"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div>
+                                    <p className="text-md">
+                                      Zemlak, Daniel and Leannon
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                              <h3 className="font-bold text-3xl text-center p-10 ">
+                                Lagu akan Hits!
+                              </h3>
+                            </div>
+                          </dialog>
+                        </td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
+                )}
               </div>
             </div>
           </div>
